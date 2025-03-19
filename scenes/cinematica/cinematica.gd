3c51a1b0e797e6c3079362d239f02d61b79499a9
jdam <jdam5000@gmail.com>
2025-03-19 19:28:15 -0400

    CInematica.gd comentado

--- scenes/cinematica/cinematica.gd
+++ scenes/cinematica/cinematica.gd
@@ -1,30 +1,32 @@
 extends Node2D
 
-@export var historia: Array[String]
-@onready var nodo_texto = $texto
 
-# Called when the node enters the scene tree for the first time.
+@export var historia: Array[String] # Arreglo que guardara 
+@onready var nodo_texto = $texto  # Una referencia directa al nodo texto 
+
 func _ready() -> void:
-	$salida.visible = false
+	# Se muestra la transición de entrada
+	$salida.visible = false 
 	$entrada.start("desvanecer")
 	$stop.start()
 	await $stop.timeout
+	
+	# Se muestra el texto de la cinematica
 	$entrada.visible = false
 	await mostrar_texto() 
+	
+	# Se muestra la trancisión de salida
 	$salida.visible = true
 	$salida.start("desvanecer invertido")
 	$stop.start()
 	await $stop.timeout
 	$salida.visible = false
 	
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 # Muestra el texto poco a poco en la caja de texto 
 func mostrar_texto():
+	# Se itera frase por frase
 	for texto in historia:
+		# Se itera letra por letra para dar el efecto de imprenta
 		for letra in texto:
 			$timer.start()
 			nodo_texto.text = nodo_texto.text + letra
@@ -32,7 +34,6 @@ func mostrar_texto():
 			await $timer.timeout
 		await esperar_input()
 		nodo_texto.text = ""
-		
 	return true
 
 # Función para esperar la entrada del jugador.

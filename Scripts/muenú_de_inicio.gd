dec23a6bb8423f82b7a47bc6a65e59983774a558
Angie8a-star <ochoaangie014@gmail.com>
2025-03-21 15:01:44 -0400

    finalizada la escena de la ventana de instrucciones

--- Scripts/muenú_de_inicio.gd
+++ Scripts/muenú_de_inicio.gd
@@ -8,9 +8,14 @@ func _on_start_pressed():
 
 
 func _on_instructions_pressed():
-	get_tree().change_scene_to_file("res://scenes/instrucciones.tscn")
+	$CanvasLayer.visible = true  # Muestra el Pop-Up
+	reproducir_animaciones()
 
-
+func _on_close_pressed():
+	$CanvasLayer.visible = false  # Oculta el Pop-Up
 
 func _on_about_us_pressed():
 	get_tree().change_scene_to_file("res://scenes/sobre_nosotros.tscn")
+
+func reproducir_animaciones():
+	$CanvasLayer/AnimationPlayer.play("RESET")

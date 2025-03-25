6419ba634d7ed9f192f1aafe73ec5162158903b9
Angie8a-star <ochoaangie014@gmail.com>
2025-03-25 15:56:36 -0400

    se añadió la escena sobre nosotros

--- Scripts/muenú_de_inicio.gd
+++ Scripts/muenú_de_inicio.gd
@@ -1,25 +1,52 @@
 extends Control
 
 
-
+#muestra de transiion al iniciar el suego (cuando se presiona jugar)
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://scenes/juego.tscn")
+	# Se muestra la trancisión de salida
+	$salida.visible = true
+	$salida.start("desvanecer invertido")
+	$stop.start()
+	await $stop.timeout
+	$salida.visible = false
+	$salida.queue_free()
+
+	get_tree().change_scene_to_file("res://scenes/cinematica.tscn")
 
 
-
+#entrada a la ventana de instrucciones
 func _on_instructions_pressed():
-	$CanvasLayer.visible = true  # Muestra el Pop-Up
+	$Instrucciones.visible = true  # Muestra el Pop-Up
 	reproducir_animaciones()
+	
+#salida de la ventana de instrucciones
+func _on_salir_intrucciones_pressed():
+	$Instrucciones.visible = false  # Oculta el Pop-Up
 
-func _on_close_pressed():
-	$CanvasLayer.visible = false  # Oculta el Pop-Up
-
-func _on_about_us_pressed():
-	get_tree().change_scene_to_file("res://scenes/sobre_nosotros.tscn")
-
+#reprodccion de animaciones de la ventanade instrucciones
 func reproducir_animaciones():
-	$CanvasLayer/AnimationPlayer.play("RESET")
-
+	$Instrucciones/AnimationPlayer.play("RESET")
 
 func _on_replay_pressed():
-	$CanvasLayer/AnimationPlayer.play("RESET")
+	$Instrucciones/AnimationPlayer.play("RESET")
+	
+#entrada a la ventana sobre nosostros
+func _on_about_us_pressed():
+	$"Sobre Nosotros".visible = true
+	
+#salida de la ventana sobre nosotros
+func _on_salir_sobre_nosotros_pressed():
+	$"Sobre Nosotros".visible = false
+
+#muetra de transicion pantalla de inicio
+func _on_ready():
+	
+	$salida.visible = false 
+	$entrada.start("desvanecer")
+	$stop.start()
+	await $stop.timeout
+	
+	# Se muestra la interfaz del juego
+	$entrada.visible = false
+	$entrada.queue_free()
+	
